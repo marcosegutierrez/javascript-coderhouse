512078c95b46f9a6a380c103b28e597dd016851e
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-12-01 23:18:37 -0300

    Aleatoriedad en muestra de comidas

--- js/comidas.js
+++ js/comidas.js
@@ -31,8 +31,11 @@ const vistaComidas = (...listado) => {
 
 const obtenerComidas = async () => {
     const alimentos = await obtenerAlimentos();
-    let listado1 = listadoComidas(alimentos, 500, 0, 2, 4, 6);
-    let listado2 = listadoComidas(alimentos, 250, 9, 10, 12);
+    let random = Math.random() * (3 - 1) + 1
+    let listado1 = (random > 2) ? 
+        listadoComidas(alimentos, 500, 0, 2, 4, 6) : listadoComidas(alimentos, 400, 1, 4, 5, 6);
+    let listado2 = (Math.trunc(random*10) % 2 == 0) ?
+        listadoComidas(alimentos, 250, 9, 10, 12) : listadoComidas(alimentos, 400, 8, 12, 7, 11);
     vistaComidas(listado1, listado2)
 }
 
