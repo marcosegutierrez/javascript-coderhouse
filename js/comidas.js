390a2301c81e4654edfebbd86eea2531ec08e338
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-12-06 22:41:51 -0300

    Manejo de errores

--- js/comidas.js
+++ js/comidas.js
@@ -1,7 +1,15 @@
 const obtenerAlimentos = async () => {
-    const result = await fetch('./alimentos.json');
-    const valores = await result.json()
-    return valores.alimentos
+    try {
+        const result = await fetch('./alimentos.json');
+        if (!result.ok) {
+            throw new Error(`Error al obtener alimentos. Código de estado: ${result.status}`);
+        }
+        const valores = await result.json();
+        return valores.alimentos;
+    } catch (error) {
+        console.error('Error en obtenerAlimentos:', error.message);
+        throw error;
+    }
 }
 
 /* Ejemplos de comidas */
@@ -30,14 +38,19 @@ const vistaComidas = (...listado) => {
     document.body.append(textAlimentos);
 }
 
-const obtenerComidas = async () => {
-    const alimentos = await obtenerAlimentos();
-    let random = Math.random() * (3 - 1) + 1
-    let listado1 = (random > 2) ? 
-        listadoComidas(alimentos, 500, 0, 2, 4, 6) : listadoComidas(alimentos, 400, 1, 4, 5, 6);
-    let listado2 = (Math.trunc(random*10) % 2 == 0) ?
-        listadoComidas(alimentos, 250, 9, 10, 12) : listadoComidas(alimentos, 400, 8, 12, 7, 11);
-    vistaComidas(listado1, listado2)
+const obtenerComidas = () => {
+    obtenerAlimentos()
+    .then( (res) => {
+        let alimentos = res;
+        let random = Math.random() * (3 - 1) + 1;
+        let listado1 = (random > 2) ? 
+            listadoComidas(alimentos, 500, 0, 2, 4, 6) : listadoComidas(alimentos, 400, 1, 4, 5, 6);
+        let listado2 = (Math.trunc(random*10) % 2 === 0) ?
+            listadoComidas(alimentos, 250, 9, 10, 12) : listadoComidas(alimentos, 400, 8, 12, 7, 11);
+        vistaComidas(listado1, listado2);
+        }
+    )
+    .catch( (error) => console.log('Error: ', error));
 }
 
 const comidas = {
